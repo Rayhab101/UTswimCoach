b27c494b92663b4c8d4d60b3e5063b1ce57cc6b6
Rayhab101 <lund64311@gmail.com>
2022-03-06 02:06:02 -0700

    commiting

--- db/update_school_descriptions.sql
+++ db/update_school_descriptions.sql
@@ -1,1 +1,1 @@
-UPDATE schools SET about=$1, short_description=$2 WHERE school_id=$3;
+UPDATE schools SET about=$1, short_description=$2, goldf=$3, silverf=$4, bronzef=$5, goldm=$6, silverm=$7, bronzem=$8 WHERE school_id=$9;
