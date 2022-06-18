bdb5fbc6e2e974707554c28591d2c5d977abbcbb
Rayhab101 <lund64311@gmail.com>
2022-06-18 11:59:55 -0600

    commiting

--- db/get_relay_times.sql
+++ db/get_relay_times.sql
@@ -1,1 +1,1 @@
-SELECT * FROM relay_times WHERE race=$1 AND time <> ''  AND (swimmers,time) IN (select swimmers, min(time) from relay_times group by swimmers, race) ORDER BY time;
+SELECT * FROM relay_times WHERE race=$1 AND time <> ''  AND (swimmers,time) IN (select swimmers, min(time) from relay_times group by swimmers, race) ORDER BY time LIMIT 50;
