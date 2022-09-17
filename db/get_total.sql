768c7f62224db9135c5f4bf736c63fe946dc6eac
Rayhab101 <lund64311@gmail.com>
2022-09-17 14:31:40 -0600

    commiting

--- db/get_total.sql
+++ db/get_total.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM records WHERE swimmer=$1 AND swim_year='2';
+SELECT COUNT(*) FROM records WHERE swimmer=$1 AND swim_year='3';
--- db/get_total_in_races.sql
+++ db/get_total_in_races.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM records WHERE swimmer=$1 AND race=$2 AND swim_year='2';
+SELECT COUNT(*) FROM records WHERE swimmer=$1 AND race=$2 AND swim_year='3';
